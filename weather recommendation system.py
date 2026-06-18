d8dd8e7ace11df4983784b2d9930b7177391a6cd
utsav singh <rajsinghthakur8889999@gmail.com>
2026-06-18 20:44:57 +0530

    Create weather recommendation system.py

---
+++ weather recommendation system.py
@@ -0,0 +1,10 @@
+weather = input("Enter weather: ")
+
+if weather == "summer":
+    print("Wear cotton clothes")
+elif weather == "winter":
+    print("Wear woolen clothes")
+elif weather == "rainy":
+    print("Carry an umbrella")
+else:
+    print("Invalid Weather")
